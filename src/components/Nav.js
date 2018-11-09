4db6f4de3a8e615d1f3c74bc4c7243ebae20da9d
Caleb Lee <caleb.gwl@gmail.com>
2018-11-09 11:33:50 -0600

    Dyanmically display page title

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,24 +3,32 @@ import { Link, withRouter } from 'react-router-dom';
 import '../App.css';
 
 class Nav extends Component {
-  // constructor() {
-  //   super();
-  //   this.state = {
-  //     path: window.location.pathname
-  //   }
-  // }
+  constructor() {
+    super();
+    this.formatTitle = this.formatTitle.bind(this);
+  }
+
+  formatTitle(str) {
+    str = str.replace('/', '');
+    console.log(str);
+    return str;
+  }
 
   render() {
     if (this.props.location.pathname === "/") {
       return (
         <header className="nav">
-          <div className="nav-content">
-            <Link to="/" className="nav-logo">
-              <h1>Christian Park</h1>
-            </Link>
-            <div className="d-flex flex-column text-right">
-              <Link to="/portfolio" className="nav-link">portfolio</Link>
-              <Link to="/about" className="nav-link">about</Link>
+          <div className="nav-content row align-items-center">
+            <div className="col">
+              <Link to="/" className="nav-logo">
+                <h1>Christian Park</h1>
+              </Link>
+            </div>
+            <div className="col">
+              <div className="d-flex flex-column text-right" >
+                <Link to="/portfolio" className="nav-link pt-4">portfolio</Link>
+                <Link to="/about" className="nav-link">about</Link>
+              </div>
             </div>
           </div>
         </header>
@@ -28,14 +36,18 @@ class Nav extends Component {
     } else {
       return (
         <header className="nav">
-          <div className="nav-content">
-            <Link to="/" className="nav-logo text-dark">
-              <h1>Christian Park</h1>
-            </Link>
-            <h1>Page Title</h1>
-            <div className = "d-flex flex-column text-dark text-right" >
-              <Link to="/portfolio" className="nav-link">portfolio</Link>
-              <Link to="/about" className="nav-link">about</Link>
+          <div className="nav-content row align-items-center">
+            <div className="col">
+              <Link to="/" className="nav-logo text-dark">
+                <h1>Christian Park</h1>
+              </Link>
+            </div>
+            <h5 className="page-title col mb-0 text-center">{this.formatTitle(this.props.location.pathname)}</h5>
+            <div className="col">
+              <div className="d-flex flex-column text-right" >
+                <Link to="/portfolio" className="nav-link text-dark pt-4">portfolio</Link>
+                <Link to="/about" className="nav-link text-dark">about</Link>
+              </div>
             </div>
           </div>
         </header>
